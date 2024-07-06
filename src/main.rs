b7d864535f20ba50dc96fe87fcbce66f1c70d125
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2024-07-06 08:57:38 +0200

    Add and migrate to tokio

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 #[allow(dead_code)]
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::io::{self};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     println!("Journey of a thousand miles begins with a single commit.");
 
     let addr = "localhost:8000";
 
-    let listener = TcpListener::bind(addr).expect("failed to bind to address localhost:8000");
+    let listener = TcpListener::bind(addr).await?;
 
-    for incoming_stream in listener.incoming() {
-        match incoming_stream {
-            Ok(valid_stream) => {
-                std::thread::spawn(|| handle_incoming_stream(valid_stream));
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            if let Err(e) = handle_incoming_stream(socket).await {
+                eprintln!("Failed to handle connection: {}", e);
             }
-            Err(err) => eprint!("connection failed \nerr::{}", err),
-        }
+        });
     }
 }
 
-fn handle_incoming_stream(mut stream: TcpStream) {
+async fn handle_incoming_stream(mut stream: TcpStream) -> io::Result<()> {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).await?;
 
     let request = String::from_utf8_lossy(&buffer[..]);
     println!("received request \n{}", request);
@@ -33,8 +35,8 @@ fn handle_incoming_stream(mut stream: TcpStream) {
         response
     );
 
-    stream
-        .write_all(&response.as_bytes())
-        .expect("failed to write to stream");
-    stream.flush().expect("failed to flush response")
+    stream.write_all(response.as_bytes()).await?;
+    stream.flush().await?;
+
+    Ok(())
 }
