d6ef5102eb31b19f00fbc10be261c55bd3e64f7e
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2024-07-06 10:44:16 +0200

    Add Arc<router>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(dead_code)]
 use std::io::{self};
+use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -11,19 +12,41 @@ impl Router {
         Self {}
     }
 
-    async fn listen_and_serve(&self, addr: &str) -> io::Result<()> {
+    async fn listen_and_serve(self: Arc<Self>, addr: &str) -> io::Result<()> {
         // let addr = "localhost:8000";
 
         let listener = TcpListener::bind(addr).await?;
 
         loop {
             let (socket, _) = listener.accept().await?;
+            let router_clone = Arc::clone(&self); // Clone Arc<Self> for each task
+
             tokio::spawn(async move {
-                if let Err(e) = handle_incoming_stream(socket).await {
+                if let Err(e) = router_clone.handle_incoming_stream(socket).await {
                     eprintln!("Failed to handle connection: {}", e);
                 }
             });
         }
+    }
+
+    async fn handle_incoming_stream(&self, mut stream: TcpStream) -> io::Result<()> {
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).await?;
+
+        let request = String::from_utf8_lossy(&buffer[..]);
+        println!("received request \n{}", request);
+
+        let response = "Hello from server!";
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}\n\n",
+            response.len(),
+            response
+        );
+
+        stream.write_all(response.as_bytes()).await?;
+        stream.flush().await?;
+
+        Ok(())
     }
 }
 
@@ -31,32 +54,12 @@ impl Router {
 async fn main() -> io::Result<()> {
     println!("Journey of a thousand miles begins with a single commit.");
 
-    let router = Router::new();
+    let router = Arc::new(Router::new());
 
     router
         .listen_and_serve("localhost:8000")
         .await
         .expect("failed to listen and serve");
-
-    Ok(())
-}
-
-async fn handle_incoming_stream(mut stream: TcpStream) -> io::Result<()> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).await?;
-
-    let request = String::from_utf8_lossy(&buffer[..]);
-    println!("received request \n{}", request);
-
-    let response = "Hello from server!";
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}\n\n",
-        response.len(),
-        response
-    );
-
-    stream.write_all(response.as_bytes()).await?;
-    stream.flush().await?;
 
     Ok(())
 }
